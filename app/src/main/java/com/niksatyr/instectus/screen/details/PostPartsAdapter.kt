dd621a4fc00065b341d60c5410c12579e322e80f
Nik Satyr <>
2020-06-07 13:22:56 +0300

    Support videos

--- app/src/main/java/com/niksatyr/instectus/screen/details/PostPartsAdapter.kt
+++ app/src/main/java/com/niksatyr/instectus/screen/details/PostPartsAdapter.kt
@@ -4,9 +4,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.VideoView
 import androidx.recyclerview.widget.RecyclerView
 import coil.api.load
 import com.niksatyr.instectus.R
+import com.niksatyr.instectus.model.Post
 
 class PostPartsAdapter(
     private val mediaUrls: MutableList<Pair<String, String>> = ArrayList()
@@ -29,13 +31,36 @@ class PostPartsAdapter(
     override fun getItemCount() = mediaUrls.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.imagePostPart.load(mediaUrls[position].first) {
-            placeholder(R.drawable.ic_cloud)
+        val postType = mediaUrls[position].second
+        val mediaUrl = mediaUrls[position].first
+
+        if (postType == Post.TYPE_IMAGE) loadImage(holder, mediaUrl) else loadVideo(holder, mediaUrl)
+    }
+
+    private fun loadImage(holder: ViewHolder, url: String) {
+        holder.apply {
+            videoViewPostPart.visibility = View.GONE
+            imageViewPostPart.visibility = View.VISIBLE
+
+            imageViewPostPart.load(url) {
+                placeholder(R.drawable.ic_cloud)
+            }
+        }
+    }
+
+    private fun loadVideo(holder: ViewHolder, url: String) {
+        holder.apply {
+            videoViewPostPart.visibility = View.VISIBLE
+            imageViewPostPart.visibility = View.GONE
+
+            videoViewPostPart.setVideoPath(url)
+            videoViewPostPart.start()
         }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val imagePostPart: ImageView = itemView.findViewById(R.id.ivPostPart)
+        val imageViewPostPart: ImageView = itemView.findViewById(R.id.ivPostPartImage)
+        val videoViewPostPart: VideoView = itemView.findViewById(R.id.ivPostPartVideo)
     }
 
 }
